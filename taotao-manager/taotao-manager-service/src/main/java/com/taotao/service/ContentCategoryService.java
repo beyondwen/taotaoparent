2f71b51517d2ae5465659d497defa0ce026bec17
wh <921790476@qq.com>
2018-02-28 17:26:20 +0800

    删除菜单树

---
+++ taotao-manager/taotao-manager-service/src/main/java/com/taotao/service/ContentCategoryService.java
@@ -0,0 +1,15 @@
+package com.taotao.service;
+
+import com.taotao.common.pojo.EUTreeNode;
+import com.taotao.common.pojo.TaotaoResult;
+
+import java.util.List;
+
+public interface ContentCategoryService {
+
+    List<EUTreeNode> getCategoryList(long parentId);
+
+    TaotaoResult insertContentCategory(long parentId, String name);
+
+    TaotaoResult deleteContentCategory(long id);
+}
